6d83c1a8efaba46e04d9a9fc2a7b44dcdfd0e721
action <action@github.com>
2024-09-02 15:02:37 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Aug 23 2024 14:44:06 GMT+0000 (Coordinated Universal Time)
+//Mon Sep 02 2024 15:02:37 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("带图评价晒单");
